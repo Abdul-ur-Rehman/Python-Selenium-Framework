db30ba6a645965f0db6b502e58ddb64c142af40c
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-07-31 17:00:04 +0500

    HandleChildWindow

--- HandleChildWindow.py
+++ HandleChildWindow.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
+
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/windows")
 driver.implicitly_wait(2)
@@ -9,7 +10,8 @@ driver.implicitly_wait(2)
 
 driver.find_element(By.LINK_TEXT, "Click Here").click()
 driver.switch_to.window(driver.window_handles[1])
-
-print(driver.find_element(By.TAG_NAME, "h3").text)
-
-assert "New Window" == driver.find_element(By.TAG_NAME, "h3").text
+msg = driver.find_element(By.TAG_NAME, "h3").text
+print(msg)
+driver.close()
+driver.switch_to.window(driver.window_handles[0])
+assert "New Window" == msg
