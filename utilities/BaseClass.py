8cd79992ed6877f7c00ac9b01cd10b0fcbd6f4cc
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-08-03 15:34:17 +0500

    utility package created

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -7,7 +7,7 @@ class BaseClass:
         loggerName = inspect.stack()[1][3]
         logger = logging.getLogger(loggerName)
 
-        filehandler = logging.FileHandler("logfile.log")
+        filehandler = logging.FileHandler("../pytest_testFiles/logfile.log")
         formatting = logging.Formatter("%(asctime)s: %(levelname)s: %(name)s: %(message)s")
         filehandler.setFormatter(formatting)
         logger.addHandler(filehandler)  # filehandler object
