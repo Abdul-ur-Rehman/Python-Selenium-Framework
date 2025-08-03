2afb085db9dd8a4a47d966cbdb49357cc1090a55
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-08-03 15:28:56 +0500

    pytest Test Files

---
+++ pytest_testFiles/test_FixturesData.py
@@ -0,0 +1,14 @@
+import pytest
+
+from utilities.BaseClass import BaseClass
+
+
+@pytest.mark.usefixtures("dataLoad")
+class TestExample(BaseClass):
+
+    def test_editProfile(self, dataLoad):
+        log = self.getLogger()
+        log.info(dataLoad[0])
+        log.info(dataLoad[1])
+        log.info(dataLoad[2])
+        print(dataLoad[2])
